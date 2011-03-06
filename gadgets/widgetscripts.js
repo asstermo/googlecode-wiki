f5e153fda91486b3575b2927f0a169308b2eda05
Bruno Santos <wyldckat@gmail.com>
2011-03-06 01:08:40 +0000

    A ver se é desta...

--- gadgets/widgetscripts.js
+++ gadgets/widgetscripts.js
@@ -16,46 +16,44 @@ if(!document.getElementsByClassName)
 var baseURL='http://wiki.asstermo.googlecode.com/hg/';
 var fileExt='.wiki';
 function loadWikis() {
-  if(_parsedWikiText & _wikiText)
-  {
-    var noTextLoaded = _parsedWikiText.length==0 && _wikiText.length==0;
-    var smartloads=document.getElementsByClassName("smartload");
 
-    for(var j=0;j<smartloads.length;j++)
+  var noTextLoaded = _parsedWikiText.length==0 && _wikiText.length==0;
+  var smartloads=document.getElementsByClassName("smartload");
+
+  for(var j=0;j<smartloads.length;j++)
+  {
+    var pageTitle=smartloads[j].getAttribute('title');
+    if(smartloads[j].innerHTML.length==0 && pageTitle.length>0)
     {
-      var pageTitle=smartloads[j].getAttribute('title');
-      if(smartloads[j].innerHTML.length==0 && pageTitle.length>0)
-      {
-        smartloads[j].setAttribute('title','');
-        _DW_getWikiPage(baseURL,pageTitle,fileExt);
-        document.write("sending request for wiki page...");
-        break;
-      }
-      else if(smartloads[j].innerHTML.length==0 && pageTitle.length==0 && !noTextLoaded)
-      {
-        document.write("waiting for request for wiki page...");
-        break;
-      }
-      else if(smartloads[j].innerHTML.length==0 && pageTitle.length==0)
-      {
-        smartloads[j].innerHTML=_wikiText;
-        _DW_syncPreview('http://code.google.com', 'asstermo', {'content': _wikiText, 'token': codesite_token});
-        _wikiText="";
-        document.write("sending request for wiki convertion...");
-        break;
-      }
-      else if(smartloads[j].innerHTML.length>0 && pageTitle.length==0 && !noTextLoaded)
-      {
-        document.write("waiting for request for wiki convertion...");
-        break;
-      }
-      else if(smartloads[j].innerHTML.length>0 && pageTitle.length==0)
-      {
-        smartloads[j].innerHTML=_parsedWikiText;
-        smartloads[j].setAttribute('class','');
-        smartloads[j].setAttribute('style','');
-        _parsedWikiText="";
-      }
+      smartloads[j].setAttribute('title','');
+      _DW_getWikiPage(baseURL,pageTitle,fileExt);
+      document.write("sending request for wiki page...");
+      break;
+    }
+    else if(smartloads[j].innerHTML.length==0 && pageTitle.length==0 && !noTextLoaded)
+    {
+      document.write("waiting for request for wiki page...");
+      break;
+    }
+    else if(smartloads[j].innerHTML.length==0 && pageTitle.length==0)
+    {
+      smartloads[j].innerHTML=_wikiText;
+      _DW_syncPreview('http://code.google.com', 'asstermo', {'content': _wikiText, 'token': codesite_token});
+      _wikiText="";
+      document.write("sending request for wiki convertion...");
+      break;
+    }
+    else if(smartloads[j].innerHTML.length>0 && pageTitle.length==0 && !noTextLoaded)
+    {
+      document.write("waiting for request for wiki convertion...");
+      break;
+    }
+    else if(smartloads[j].innerHTML.length>0 && pageTitle.length==0)
+    {
+      smartloads[j].innerHTML=_parsedWikiText;
+      smartloads[j].setAttribute('class','');
+      smartloads[j].setAttribute('style','');
+      _parsedWikiText="";
     }
   }
   setTimeout(loadWikis, 100);
