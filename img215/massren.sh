f98c4c2212488d8cd4fd4a31db734280db21056d
Bruno Santos <wyldckat@gmail.com>
2011-03-20 14:25:33 +0000

    Primeiro commit da página de combustão.

--- img215/massren.sh
+++ img215/massren.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 x=0
-find . -name "[0-9][0-9].png" | sort -u | \
+find . -name "[0-9][0-9]*.png" | sort -u | \
 while read f; do
   x=`expr $x + 1`
-  mv $f ManualAsstermo215Psicrometria_$x.png
+  mv $f ManualAsstermo215Combustao_$x.png
 done
